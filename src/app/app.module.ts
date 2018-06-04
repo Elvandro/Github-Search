0c846afc8dd6f6605686dd897236af482fdb837e
Elvis Amuni <elvisamuni22@gmail.com>
2018-06-04 11:46:01 +0300

    This commit will create a new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { UserSearchComponent } from './user-search/user-search.component';
 import { UserDisplayComponent } from './user-display/user-display.component';
+import { RepoDisplayComponent } from './repo-display/repo-display.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserSearchComponent,
-    UserDisplayComponent
+    UserDisplayComponent,
+    RepoDisplayComponent
   ],
   imports: [
     BrowserModule
