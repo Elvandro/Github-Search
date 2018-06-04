92aebc2cff1c5c299769f2d10038798c97ddd883
Elvis Amuni <elvisamuni22@gmail.com>
2018-06-04 08:09:26 +0300

    This commit will add code to the service files

--- src/app/git.service.ts
+++ src/app/git.service.ts
@@ -33,8 +33,35 @@ export class GitService {
            this.profiles.bio = res.bio;
            this.profiles.dateCreated = res.dateCreated;
            console.log(this.profiles.dateCreated);
+
+           this.profiles.userName = res.userName;
+           this.profiles.public_repos = res.public_repos;
+           this.profiles.repo_url = res.repo_url;
+           console.log(this.profiles.userName)
          }
-       )
-     })
+       );
+     });
+     return promise;
    }
+  repoRequest(profile:string){
+    interface Repositories{
+      name:string;
+      repo_url:any;
+      description:any;
+      dateCreated:Date;
+    }
+    let promise = new Promise((resolve,reject)=>{
+      this.http.get<Repositories>(environment.apiUrl+profile+'/repos'+environment.Key).toPromise().then(
+        res=>{
+          this.results = res;
+          this.repos.repo_url = res.repos_url;
+          this.repos.description = res.description;
+          this.repos.dateCreated = res.dateCreated;
+
+          resolve();
+        }
+      );
+    });
+    return promise;
+  }
 }
